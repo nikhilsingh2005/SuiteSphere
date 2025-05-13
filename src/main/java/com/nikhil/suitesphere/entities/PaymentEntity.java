45f824628b1e7ad5557c77232abb5039d16ad18e
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-05-13 20:12:58 +0530

    Create PaymentEntity class with transaction details, payment status, and gateway information

---
+++ src/main/java/com/nikhil/suitesphere/entities/PaymentEntity.java
@@ -0,0 +1,45 @@
+package com.nikhil.suitesphere.entities;
+
+import com.nikhil.suitesphere.enums.PaymentGateway;
+import com.nikhil.suitesphere.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "payments")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String transactionId;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentGateway paymentGateway;
+
+    private LocalDateTime paymentDate;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    private String bookingReference;
+    private String failureReason;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
