6aae3d97a56c47e5cf04e047e5b331392b710d64
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-05-14 00:16:06 +0530

    Add PaymentDTO class to transfer structured payment details in API communication

---
+++ src/main/java/com/nikhil/suitesphere/dtos/PaymentDTO.java
@@ -0,0 +1,43 @@
+package com.nikhil.suitesphere.dtos;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.nikhil.suitesphere.enums.PaymentGateway;
+import com.nikhil.suitesphere.enums.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PaymentDTO {
+
+    private Long id;
+    private BookingDTO booking;
+
+    private String transactionId;
+
+    private BigDecimal amount;
+
+    private PaymentGateway paymentMethod; //e,g PayPal. Stripe, flutterwave, paystack
+
+    private LocalDateTime paymentDate;
+
+    private PaymentStatus status; //failed, e.t.c
+
+    private String bookingReference;
+    private String failureReason;
+
+    private String approvalLink; //paypal payment approval UEL
+
+}
+
