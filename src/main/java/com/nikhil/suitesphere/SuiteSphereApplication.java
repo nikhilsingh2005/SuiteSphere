6d3782e862292a4b68fdcfd499d0e1b2c44c8d67
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-05-13 19:37:15 +0530

    Initial setup with project structure, dependencies, and configuration

---
+++ src/main/java/com/nikhil/suitesphere/SuiteSphereApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.suitesphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiteSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuiteSphereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikhil/suitesphere/SuiteSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.suitesphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuiteSphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
