4638969d4135b11652eb98fabbc699144c942f14
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-05-14 01:19:01 +0530

    Add JWT-based security components and CORS configuration

---
+++ src/main/java/com/nikhil/suitesphere/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.nikhil.suitesphere.security;
+
+
+import com.nikhil.suitesphere.entities.User;
+import com.nikhil.suitesphere.exceptions.NotFoundException;
+import com.nikhil.suitesphere.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username).
+                orElseThrow(() -> new NotFoundException("User Email Not Found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
+
