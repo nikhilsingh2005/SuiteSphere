c216e62216b86970237c695b25fd63a972ac8582
Nikhil Singh <nikhilsingh20102002@gmail.com>
2025-05-14 00:11:08 +0530

    Implement DTO classes for User, Room, and Booking

--- src/main/java/com/nikhil/suitesphere/dtos/BookingDTO.java
+++ src/main/java/com/nikhil/suitesphere/dtos/BookingDTO.java
@@ -1,4 +1,43 @@
 package com.nikhil.suitesphere.dtos;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.nikhil.suitesphere.enums.BookingStatus;
+import com.nikhil.suitesphere.enums.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BookingDTO {
+
+    private Long id;
+
+    private UserDTO user;
+
+    private RoomDTO room;
+    private Long roomId;
+
+    private PaymentStatus paymentStatus;
+
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+
+    private BigDecimal totalPrice;
+    private String bookingReference;
+    private LocalDateTime createdAt;
+
+    private BookingStatus bookingStatus;
+
 }
